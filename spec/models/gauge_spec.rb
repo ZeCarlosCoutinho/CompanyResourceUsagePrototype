1c0108e09535a88bf82d86f32792f4f0f890720b
ZeCarlosCoutinho <jczelik@gmail.com>
2024-11-01 21:37:59 +0100

    test(gauge): list the tests that have to be done regarding validations

--- spec/models/gauge_spec.rb
+++ spec/models/gauge_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Gauge, type: :model do
       expect(FactoryBot.create(:gauge)).to be_valid
     end
   end
+
+  describe 'validations' do
+    # TODO Validate that none of the fields can be null
+    # TODO Validate that start date is always equal or before the end date
+    # TODO Validate that the time_slots have to be one of the enums
+    pending
+  end
 end
