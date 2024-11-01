559cfd3742c28bd95ae9740f20a5aab1c13a8c86
ZeCarlosCoutinho <jczelik@gmail.com>
2024-11-01 21:36:17 +0100

    feat(gauge): validate the presence of the mandatory attributes

--- app/models/gauge.rb
+++ app/models/gauge.rb
@@ -1,3 +1,9 @@
 class Gauge < ApplicationRecord
   enum time_slot: [ :daily, :weekly, :monthly, :yearly ]
+
+  validates :unit, presence: true
+  validates :time_slot, presence: true
+  validates :name, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
