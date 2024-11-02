11e1c66458d6350e23b1e38511d278c0cea980c3
ZeCarlosCoutinho <jczelik@gmail.com>
2024-11-02 10:28:28 +0100

    feat(gauge log): validate the presence of value and date

--- app/models/gauge_log.rb
+++ app/models/gauge_log.rb
@@ -3,6 +3,9 @@ class GaugeLog < ApplicationRecord
   belongs_to :filled_in_by, class_name: "Profile"
   belongs_to :approved_by, class_name: "Profile", optional: true
 
+  validates :value, presence: true
+  validates :date, presence: true
+
   def approve!(profile)
     return false unless profile.is_manager
 
