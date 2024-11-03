6693d3eb274b8878f9354003a6d01497dca74ac2
ZeCarlosCoutinho <jczelik@gmail.com>
2024-11-03 20:49:21 +0100

    test(gauge log): test that a user is not allowed to log a value for an existing day

--- spec/system/log_values_system_spec.rb
+++ spec/system/log_values_system_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe "Value Logging", type: :system do
   let(:start_date) { 1.month.ago }
   let(:end_date) { Date.today.next_month }
   let!(:target_gauge) { FactoryBot.create(:gauge, start_date: start_date, end_date: end_date) }
+  
+  let(:existing_date) { Date.today }
+  let!(:existing_log) { FactoryBot.create(:gauge_log, gauge: target_gauge, date: existing_date) }
 
   subject { profile.user }
   before(:each) { sign_in(subject) }
@@ -23,7 +26,7 @@ RSpec.describe "Value Logging", type: :system do
       expect(page).to have_button("Add")
 
       chosen_value = 42
-      chosen_date = Date.today
+      chosen_date = existing_date - 1.day
       fill_in "add-value-input", with: chosen_value
       fill_in "add-date-input", with: chosen_date.to_fs
 
@@ -31,6 +34,16 @@ RSpec.describe "Value Logging", type: :system do
       new_gauge_log = target_gauge.gauge_logs.find_by(date: chosen_date)
       expect(new_gauge_log).to_not be_nil
       expect(new_gauge_log.value).to eq(chosen_value)
+    end
+
+    it 'does not allow to log a value for a date that already has a log' do
+      visit "/gauge/show?id=#{target_gauge.id}"
+
+      chosen_value = 42
+      chosen_date = existing_date
+      fill_in "add-value-input", with: chosen_value
+      fill_in "add-date-input", with: chosen_date.to_fs
+      expect { click_button("Add") }.to_not change(GaugeLog, :count)
     end
   end
 end
