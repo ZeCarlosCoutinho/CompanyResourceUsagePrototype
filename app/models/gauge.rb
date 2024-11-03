56bd292a2533656e06de63bd8de8b027d9ec2eb0
ZeCarlosCoutinho <jczelik@gmail.com>
2024-11-03 13:30:08 +0100

    feat(gauge): validate that the end date comes after the start date

--- app/models/gauge.rb
+++ app/models/gauge.rb
@@ -15,4 +15,14 @@ class Gauge < ApplicationRecord
   validates :name, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  validate :end_is_after_start
+
+  private
+
+  def end_is_after_start
+    return if end_date >= start_date
+
+    errors.add(:end_date, "The end date must be the same or after the start date")
+  end
 end
