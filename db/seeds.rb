e9ff32fc86ece14a0f8ab77c8fa31f3ab4292e4d
ZeCarlosCoutinho <jczelik@gmail.com>
2024-11-04 02:58:41 +0100

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+employee1_email = 'employee1@example.com'
+employee1 = User.find_by(email: employee1_email)
+employee1 ||= User.new(email: employee1_email, password: 'testtest')
+# employee1.skip_confirmation!
+employee1_profile = employee1.profile
+employee1_profile ||= Profile.create(name: "Darth Vader", is_manager: false, user: employee1)
+employee1.save!
+
+employee2_email = 'employee2@example.com'
+employee2 = User.find_by(email: employee2_email)
+employee2 ||= User.new(email: employee2_email, password: 'testtest')
+# employee2.skip_confirmation!
+employee2_profile = employee2.profile
+employee2_profile ||= Profile.create(name: "Darth Maul", is_manager: false, user: employee2)
+employee2.save!
+
+manager1_email = 'employee3@example.com'
+manager1 = User.find_by(email: manager1_email)
+manager1 ||= User.new(email: manager1_email, password: 'testtest')
+# manager1.skip_confirmation!
+manager1_profile = manager1.profile
+manager1_profile ||= Profile.create(name: "Emperor Palpatine", is_manager: true, user: manager1)
+manager1.save!
+
+manager2_email = 'employee4@example.com'
+manager2 = User.find_by(email: manager2_email)
+manager2 ||= User.new(email: manager2_email, password: 'testtest')
+# manager2.skip_confirmation!
+manager2_profile = manager2.profile
+manager2_profile ||= Profile.create(name: "Darth Sidious", is_manager: true, user: manager2)
+manager2.save!
+
+gauge1 = Gauge.find_by(name: "Test Gauge 1")
+gauge1 ||= Gauge.new(name: "Test Gauge 1",
+                  unit: 'kWh',
+                  time_slot: Gauge.time_slots[:daily],
+                  start_date: 1.month.ago,
+                  end_date: Date.today.next_month)
+gauge1.save!
