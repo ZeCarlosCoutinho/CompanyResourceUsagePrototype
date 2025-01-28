0fbdfd49c1b317488fd08ef8795139fd9fad6fb7
ZeCarlosCoutinho <jczelik@gmail.com>
2025-01-28 14:40:11 +0100

    test(gauges controller): remove time slots from the tests

--- spec/controllers/gauges_controller_spec.rb
+++ spec/controllers/gauges_controller_spec.rb
@@ -9,26 +9,22 @@ RSpec.describe GaugesController, type: :controller do
 
     let(:unit1) { 'testunit' }
     let(:name1) { 'testname' }
-    let(:timeslot1) { Gauge.time_slots[:daily] }
     let(:start_date1) { Time.zone.yesterday }
     let(:end_date1) { Time.zone.today }
     let!(:gauge1) do
       FactoryBot.create(:gauge, unit: unit1,
                                 name: name1,
-                                time_slot: timeslot1,
                                 start_date: start_date1,
                                 end_date: end_date1)
     end
 
     let(:unit2) { 'testunit2' }
     let(:name2) { 'testname2' }
-    let(:timeslot2) { Gauge.time_slots[:weekly] }
     let(:start_date2) { Time.zone.yesterday - 1.week }
     let(:end_date2) { Time.zone.today - 1.week }
     let!(:gauge2) do
       FactoryBot.create(:gauge, unit: unit2,
                                 name: name2,
-                                time_slot: timeslot2,
                                 start_date: start_date2,
                                 end_date: end_date2)
     end
