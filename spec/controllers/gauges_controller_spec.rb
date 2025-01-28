b602546e7946a8a4450386ad4298e2a4cd5a0c93
ZeCarlosCoutinho <jczelik@gmail.com>
2025-01-28 13:20:29 +0100

    test(gauges controller): add tests for the happy flow

--- spec/controllers/gauges_controller_spec.rb
+++ spec/controllers/gauges_controller_spec.rb
@@ -4,14 +4,15 @@ RSpec.describe GaugesController, type: :controller do
   before(:each) { sign_in current_profile.user }
 
   describe 'GET index' do
+    render_views
     let(:unit1) { 'testunit' }
     let(:name1) { 'testname' }
     let(:timeslot1) { Gauge.time_slots[:daily] }
     let(:start_date1) { Time.zone.yesterday }
     let(:end_date1) { Time.zone.today }
-    let(:gauge1) do
-      FactoryBot.create(:gauge, unit: testunit1,
-                                name: testname1,
+    let!(:gauge1) do
+      FactoryBot.create(:gauge, unit: unit1,
+                                name: name1,
                                 time_slot: timeslot1,
                                 start_date: start_date1,
                                 end_date: end_date1)
@@ -22,9 +23,9 @@ RSpec.describe GaugesController, type: :controller do
     let(:timeslot2) { Gauge.time_slots[:weekly] }
     let(:start_date2) { Time.zone.yesterday - 1.week }
     let(:end_date2) { Time.zone.today - 1.week }
-    let(:gauge2) do
-      FactoryBot.create(:gauge, unit: testunit2,
-                                name: testname2,
+    let!(:gauge2) do
+      FactoryBot.create(:gauge, unit: unit2,
+                                name: name2,
                                 time_slot: timeslot2,
                                 start_date: start_date2,
                                 end_date: end_date2)
@@ -33,13 +34,23 @@ RSpec.describe GaugesController, type: :controller do
     let(:current_profile) { FactoryBot.create(:manager) }
     subject { get "index" }
 
-    it 'lists all the gauges' do
-      # TODO
-    end
-
-    it 'returns a 200 status code', focus: true do
+    it 'returns a 200 status code' do
       subject
       expect(response.status).to eq(200)
+    end
+
+    it 'lists all the gauges' do
+      subject
+      rendered_html = response.body
+      expect(rendered_html).to match(/<h3>#{name1}<\/h3>/)
+      expect(rendered_html).to match(/<li>Units: #{unit1}<\/li>/)
+      expect(rendered_html).to match(/<li>Start Date: #{start_date1}<\/li>/)
+      expect(rendered_html).to match(/<li>End Date: #{end_date1}<\/li>/)
+
+      expect(rendered_html).to match(/<h3>#{name2}<\/h3>/)
+      expect(rendered_html).to match(/<li>Units: #{unit2}<\/li>/)
+      expect(rendered_html).to match(/<li>Start Date: #{start_date2}<\/li>/)
+      expect(rendered_html).to match(/<li>End Date: #{end_date2}<\/li>/)
     end
   end
 end
